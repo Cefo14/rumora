c4cc6d48b5d163bb91e7882ac694ce74fba48219
Cefo14 <cefo14@protonmail.com>
2025-08-19 18:07:01 -0600

    feat: initialize project with TypeScript and Vite configuration

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
+
+const rootDir = resolve(__dirname, 'src')
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(rootDir, 'main.ts'),
+      name: 'Rumora',
+      formats: ['es', 'cjs'],
+      fileName: (format) => `rumora.${format}.js`
+    },
+    sourcemap: true,
+    outDir: 'dist'
+  },
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+      outDir: 'dist'
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': rootDir
+    }
+  }
+})
